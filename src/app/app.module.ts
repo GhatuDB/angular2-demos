900d6235ee4b4285cb32378f45e2906cbe902c54
GhatuDB <ghatudb@gmail.com>
2019-10-07 22:34:04 +0530

    2 files modified, 4 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, TemplateFormComponent, ReactiveFormComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
